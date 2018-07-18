570aed31567de70005d77e11c84ab05472885e25
jneilliii <jneilliii+github@gmail.com>
2018-07-18 18:08:26 -0400

    0.1.0

---
+++ README.md
@@ -0,0 +1,51 @@
+# OctoPrint-WemoSwitch
+
+Work inspired by [OctoPrint-PSUControl](https://github.com/kantlivelong/OctoPrint-PSUControl) and [TP-Link WiFi SmartPlug Client](https://github.com/softScheck/tplink-smartplug), this plugin controls a TP-Link Smartplug via OctoPrint's nav bar. 
+
+##  Screenshots
+![screenshot](screenshot.png)
+
+![screenshot](settings.png)
+
+![screenshot](plugeditor.png)
+
+## Setup
+
+Install via the bundled [Plugin Manager](https://github.com/foosel/OctoPrint/wiki/Plugin:-Plugin-Manager)
+or manually using this URL:
+
+    https://github.com/jneilliii/OctoPrint-WemoSwitch/archive/master.zip
+
+
+## Configuration
+
+Once installed go into settings and enter the ip address for your TP-Link Smartplug device. Adjust additional settings as needed.
+
+## Settings Explained
+- **IP**
+  - IP or hostname of plug to control.
+- **Label**
+  - Label to use for title attribute on hover over button in navbar.
+- **Icon Class**
+  - Class name from [fontawesome](http://fontawesome.io/3.2.1/cheatsheet/) to use for icon on button.
+- **Warn**
+  - The left checkbox will always warn when checked.
+  - The right checkbox will only warn when printer is printing.
+- **GCODE**
+  - When checked this will enable the processing of M80 and M81 commands from gcode to power on/off plug.  Syntax for gcode command is M80/M81 followed by hostname/ip.  For example if your plug is 192.168.1.2 your gcode command would be **M80 192.168.1.2**
+- **postConnect**
+  - Automatically connect to printer after plug is powered on.
+  - Will wait for number of seconds configured in **Auto Connect Delay** setting prior to attempting connection to printer.
+- **preDisconnect**
+  - Automatically disconnect printer prior to powering off the plug.
+  - Will wait for number of seconds configured in **Auto Disconnect Delay** prior to powering off the plug.
+- **Cmd On**
+  - When checked will run system command configured in **System Command On** setting after a delay in seconds configured in **System Command On Delay**.
+- **Cmd Off**
+  - When checked will run system command configured in **System Command Off** setting after a delay in seconds configured in **System Command Off Delay**.
+  
+## Support My Efforts
+I programmed this plugin for fun and do my best effort to support those that have issues with it, please return the favor and support me.
+
+[![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://paypal.me/jneilliii)
+
---
+++ octoprint_wemoswitch/__init__.py
@@ -0,0 +1,256 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+from octoprint.server import user_permission
+import socket
+import json
+import logging
+import os
+import re
+import threading
+import time
+import pywemo
+
+class wemoswitchPlugin(octoprint.plugin.SettingsPlugin,
+                            octoprint.plugin.AssetPlugin,
+                            octoprint.plugin.TemplatePlugin,
+							octoprint.plugin.SimpleApiPlugin,
+							octoprint.plugin.StartupPlugin):
+							
+	def __init__(self):
+		self._logger = logging.getLogger("octoprint.plugins.wemoswitch")
+		self._wemoswitch_logger = logging.getLogger("octoprint.plugins.wemoswitch.debug")
+							
+	##~~ StartupPlugin mixin
+	
+	def on_startup(self, host, port):
+		# setup customized logger
+		from octoprint.logging.handlers import CleaningTimedRotatingFileHandler
+		wemoswitch_logging_handler = CleaningTimedRotatingFileHandler(self._settings.get_plugin_logfile_path(postfix="debug"), when="D", backupCount=3)
+		wemoswitch_logging_handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s"))
+		wemoswitch_logging_handler.setLevel(logging.DEBUG)
+
+		self._wemoswitch_logger.addHandler(wemoswitch_logging_handler)
+		self._wemoswitch_logger.setLevel(logging.DEBUG if self._settings.get_boolean(["debug_logging"]) else logging.INFO)
+		self._wemoswitch_logger.propagate = False
+	
+	def on_after_startup(self):
+		self._logger.info("WemoSwitch loaded!")
+	
+	##~~ SettingsPlugin mixin
+	
+	def get_settings_defaults(self):
+		return dict(
+			debug_logging = False,
+			arrSmartplugs = [{'ip':'','label':'','icon':'icon-bolt','displayWarning':True,'warnPrinting':False,'gcodeEnabled':False,'gcodeOnDelay':0,'gcodeOffDelay':0,'autoConnect':True,'autoConnectDelay':10.0,'autoDisconnect':True,'autoDisconnectDelay':0,'sysCmdOn':False,'sysRunCmdOn':'','sysCmdOnDelay':0,'sysCmdOff':False,'sysRunCmdOff':'','sysCmdOffDelay':0,'currentState':'unknown','btnColor':'#808080'}],
+			pollingInterval = 15,
+			pollingEnabled = False
+		)
+		
+	def on_settings_save(self, data):	
+		old_debug_logging = self._settings.get_boolean(["debug_logging"])
+
+		octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
+
+		new_debug_logging = self._settings.get_boolean(["debug_logging"])
+		if old_debug_logging != new_debug_logging:
+			if new_debug_logging:
+				self._wemoswitch_logger.setLevel(logging.DEBUG)
+			else:
+				self._wemoswitch_logger.setLevel(logging.INFO)
+				
+	def get_settings_version(self):
+		return 1
+		
+	def on_settings_migrate(self, target, current=None):
+		if current is None or current < self.get_settings_version():
+			# Reset plug settings to defaults.
+			self._logger.debug("Resetting arrSmartplugs for wemoswitch settings.")
+			self._settings.set(['arrSmartplugs'], self.get_settings_defaults()["arrSmartplugs"])
+		
+	##~~ AssetPlugin mixin
+
+	def get_assets(self):
+		return dict(
+			js=["js/wemoswitch.js"],
+			css=["css/wemoswitch.css"]
+		)
+		
+	##~~ TemplatePlugin mixin
+	
+	def get_template_configs(self):
+		return [
+			dict(type="navbar", custom_bindings=True),
+			dict(type="settings", custom_bindings=True)
+		]
+		
+	##~~ SimpleApiPlugin mixin
+	
+	def turn_on(self, plugip):
+		self._wemoswitch_logger.debug("Turning on %s." % plugip)
+		plug = self.plug_search(self._settings.get(["arrSmartplugs"]),"ip",plugip)
+		self._wemoswitch_logger.debug(plug)		
+		chk = self.sendCommand("on",plugip)
+		if chk == 0:
+			self.check_status(plugip)
+			if plug["autoConnect"]:
+				t = threading.Timer(int(plug["autoConnectDelay"]),self._printer.connect)
+				t.start()
+			if plug["sysCmdOn"]:
+				t = threading.Timer(int(plug["sysCmdOnDelay"]),os.system,args=[plug["sysRunCmdOn"]])
+				t.start()
+	
+	def turn_off(self, plugip):
+		self._wemoswitch_logger.debug("Turning off %s." % plugip)
+		plug = self.plug_search(self._settings.get(["arrSmartplugs"]),"ip",plugip)
+		self._wemoswitch_logger.debug(plug)
+		if plug["sysCmdOff"]:
+			t = threading.Timer(int(plug["sysCmdOffDelay"]),os.system,args=[plug["sysRunCmdOff"]])
+			t.start()			
+		if plug["autoDisconnect"]:
+			self._printer.disconnect()
+			time.sleep(int(plug["autoDisconnectDelay"]))
+		chk = self.sendCommand("off",plugip)
+		if chk == 0:
+			self.check_status(plugip)
+		
+	def check_status(self, plugip):
+		self._wemoswitch_logger.debug("Checking status of %s." % plugip)
+		if plugip != "":
+			chk = self.sendCommand("info",plugip)
+			if chk == 1:
+				self._plugin_manager.send_plugin_message(self._identifier, dict(currentState="on",ip=plugip))
+			elif chk == 0:
+				self._plugin_manager.send_plugin_message(self._identifier, dict(currentState="off",ip=plugip))
+			else:
+				self._wemoswitch_logger.debug(chk)
+				self._plugin_manager.send_plugin_message(self._identifier, dict(currentState="unknown",ip=plugip))		
+	
+	def get_api_commands(self):
+		return dict(turnOn=["ip"],turnOff=["ip"],checkStatus=["ip"])
+
+	def on_api_command(self, command, data):
+		if not user_permission.can():
+			from flask import make_response
+			return make_response("Insufficient rights", 403)
+        
+		if command == 'turnOn':
+			self.turn_on("{ip}".format(**data))
+		elif command == 'turnOff':
+			self.turn_off("{ip}".format(**data))
+		elif command == 'checkStatus':
+			self.check_status("{ip}".format(**data))
+			
+	##~~ Utilities
+	
+	def plug_search(self, list, key, value): 
+		for item in list: 
+			if item[key] == value: 
+				return item
+	
+	def sendCommand(self, cmd, plugip):		
+		# try to connect via ip address
+		try:
+			socket.inet_aton(plugip)
+			ip = plugip
+			self._wemoswitch_logger.debug("IP %s is valid." % plugip)
+		except socket.error:
+		# try to convert hostname to ip
+			self._wemoswitch_logger.debug("Invalid ip %s trying hostname." % plugip)
+			try:
+				ip = socket.gethostbyname(plugip)
+				self._wemoswitch_logger.debug("Hostname %s is valid." % plugip)
+			except (socket.herror, socket.gaierror):
+				self._wemoswitch_logger.debug("Invalid hostname %s." % plugip)
+				return 3
+				
+		try:
+			self._wemoswitch_logger.debug("Attempting to connect to %s" % plugip)
+			device = pywemo.discovery.device_from_description("http://%s/setup.xml" % plugip, None)			
+			
+			self._wemoswitch_logger.debug("Sending command %s to %s" % (cmd,plugip))
+			
+			if cmd == "info":
+				return device.get_state()
+			elif cmd == "on":
+				device.on()
+				return 0
+			elif cmd == "off":
+				device.off()
+				return 0
+				
+		except socket.error:
+			self._wemoswitch_logger.debug("Could not connect to %s." % plugip)
+			return 3
+			
+	##~~ Gcode processing hook
+	
+	def gcode_turn_off(self, plug):
+		if plug["warnPrinting"] and self._printer.is_printing():
+			self._logger.info("Not powering off %s because printer is printing." % plug["label"])
+		else:
+			self.turn_off(plug["ip"])
+	
+	def processGCODE(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+		if gcode:
+			if cmd.startswith("M80"):			
+				plugip = re.sub(r'^M80\s?', '', cmd)
+				self._wemoswitch_logger.debug("Received M80 command, attempting power on of %s." % plugip)
+				plug = self.plug_search(self._settings.get(["arrSmartplugs"]),"ip",plugip)
+				self._wemoswitch_logger.debug(plug)
+				if plug["gcodeEnabled"]:
+					t = threading.Timer(int(plug["gcodeOnDelay"]),self.turn_on,args=[plugip])
+					t.start()
+				return
+			elif cmd.startswith("M81"):
+				plugip = re.sub(r'^M81\s?', '', cmd)
+				self._wemoswitch_logger.debug("Received M81 command, attempting power off of %s." % plugip)
+				plug = self.plug_search(self._settings.get(["arrSmartplugs"]),"ip",plugip)
+				self._wemoswitch_logger.debug(plug)
+				if plug["gcodeEnabled"]:
+					t = threading.Timer(int(plug["gcodeOffDelay"]),self.gcode_turn_off,[plug])
+					t.start()
+				return
+			else:
+				return
+			
+
+	##~~ Softwareupdate hook
+
+	def get_update_information(self):
+		# Define the configuration for your plugin to use with the Software Update
+		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
+		# for details.
+		return dict(
+			wemoswitch=dict(
+				displayName="Wemo Switch",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="jneilliii",
+				repo="OctoPrint-WemoSwitch",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/jneilliii/OctoPrint-WemoSwitch/archive/{target_version}.zip"
+			)
+		)
+
+
+# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
+# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
+# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
+__plugin_name__ = "Wemo Switch"
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = wemoswitchPlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.processGCODE,
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
+
