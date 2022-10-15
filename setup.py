dded8645875bb2e5c844849a9f98cd1a7a1d3b72
jneilliii <jneilliii+github@gmail.com>
2022-10-15 11:30:17 -0400

    0.1.11

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_wemoswitch"
 plugin_name = "OctoPrint-WemoSwitch"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.11rc2"
+plugin_version = "0.1.11"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
